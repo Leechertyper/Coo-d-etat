97e70923f1ecd172a0a03f20c0f839852944298d
Noah V <noahvonk28@gmail.com>
2023-02-07 21:10:34 -0600

    Added Skeleton Functions for Damage to Explosions Script

--- Assets/Scripts/Bosses/explosion.cs
+++ Assets/Scripts/Bosses/explosion.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class explosion : MonoBehaviour
+public class Explosion : MonoBehaviour
 {
     [SerializeField] private GameObject parent;
+
+    private float _damage;
+
+    private void Start()
+    {
+        _damage = 50; // this will be taken from the globals once the server is up and running
+    }
 
     /// <summary>
     /// When the animation ends, destroy self
@@ -13,5 +20,14 @@ public class explosion : MonoBehaviour
     {
         //check if player is in explosion
         Destroy(parent);
+    }
+
+    /// <summary>
+    /// Called every frame
+    /// </summary>
+    private void Update()
+    {
+        // will have it check for player collision and deal damage using the players "damage" function
+        // GameManager.instance.player.Damage(_damage);
     }
 }
