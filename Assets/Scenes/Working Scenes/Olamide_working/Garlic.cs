e25584fee73340dcf88c61f4e98cce4abb0f0892
Olamide <odd226@usask.ca>
2023-02-09 00:49:42 -0600

    Added Generic Item

---
+++ Assets/Scenes/Working Scenes/Olamide_working/Garlic.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Items/Garlic")]
+public class Garlic : ItemEffect
+{
+    //the amount that a stat changes 
+    //after garlic is picked up
+    public float amount;
+    public override void Apply(GameObject target)
+    {
+        //to do
+
+    }
+}
