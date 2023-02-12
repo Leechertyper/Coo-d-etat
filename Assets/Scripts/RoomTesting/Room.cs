3b85cebfb90df6985445aad300488c19e5384bde
Noah <36053916+noahbesse@users.noreply.github.com>
2023-02-12 15:14:23 -0600

    Basic Room
    Changed room scale and changed room to tilemap

---
+++ Assets/Scripts/RoomTesting/Room.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public bool cleared;
+    
+    
+}
--- Assets/Scripts/RoomTesting/RoomDoor.cs
+++ Assets/Scripts/RoomTesting/RoomDoor.cs
@@ -3,5 +3,22 @@ using UnityEngine;
 public class RoomDoor : MonoBehaviour
 {
     [SerializeField] private bool locked;
+    [SerializeField] private Sprite doorSpriteUnlocked;
+    [SerializeField] private Sprite doorSpriteLocked;
+    
+    private SpriteRenderer _doorSpriteRenderer;
 
+    private void Start()
+    {
+        _doorSpriteRenderer = GetComponent<SpriteRenderer>();
+        _doorSpriteRenderer.sprite = locked ? doorSpriteLocked : doorSpriteUnlocked;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player") || !locked)
+        {
+            //TODO: move player to next room
+        }
+    }
 }
