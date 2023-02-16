e4924aefc014707611008387cf30f087e97689ac
Noah <36053916+noahbesse@users.noreply.github.com>
2023-02-15 18:34:19 -0600

    Changed all the room art assets + fixed alignment issues
    floor prefab now goes at 0,0. Main camera needs the camera controller script but no longer needs to be dragged into the floor prefab.

--- Assets/Scripts/RoomTesting/RoomTestPlayer.cs
+++ Assets/Scripts/RoomTesting/RoomTestPlayer.cs
@@ -5,20 +5,19 @@ public class RoomTestPlayer : MonoBehaviour
     // THROW THIS ON SOMETHING THAT HAS A RIGIDBODY2D AND A COLLIDER (MAKE SURE IT IS TAGGED PLAYER)
     
     private Rigidbody2D _rb;
-    private float _verticalInput;
-    private float _horizontalInput;
     [SerializeField] private float moveSpeed = 20f;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _verticalInput = Input.GetAxisRaw("Vertical");
-        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        
     }
 
     private void Update()
     {
-        var newVelocity = new Vector3(_horizontalInput * moveSpeed, _verticalInput * moveSpeed);
+        var verticalInput = Input.GetAxisRaw("Vertical");
+        var horizontalInput = Input.GetAxisRaw("Horizontal");
+        var newVelocity = new Vector3(horizontalInput * moveSpeed, verticalInput * moveSpeed);
         _rb.velocity = newVelocity;
     }
 }
