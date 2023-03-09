511dbaf1564b33a9fabe5402c3cb98ab14166117
LiamHilkewich <liamhilkewich@gmail.com>
2023-03-09 03:46:22 -0600

    Added comments, and now displays high score

--- Assets/Scenes/Working Scenes/Liam_working/Score.cs
+++ Assets/Scenes/Working Scenes/Liam_working/Score.cs
@@ -5,22 +5,28 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-
     public int score = 0;
-    public GameObject scoreText;
     public int highScore = 0;
-    // Start is called before the first frame update
+
+    public GameObject scoreText;
+    public GameObject highScoreText;
+
+    // Load the high score from PlayerPrefs on start
     void Start()
     {
         LoadScore();
     }
 
-    // Update is called once per frame
+    // Display high score when 'H' key is pressed
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            DisplayHighScore();
+        }
     }
 
+    // Add score to current score, update UI, and update high score if current score is higher
     public void AddScore(int amount)
     {
         score += amount;
@@ -31,23 +37,56 @@ public class Score : MonoBehaviour
         }
     }
 
-    private void SaveScore()
-    {
-        PlayerPrefs.SetInt("HighScore", highScore);
-    }
-
-    private void LoadScore()
-    {
-        highScore = PlayerPrefs.GetInt("HighScore");
-    }
-
+    // Save the high score to PlayerPrefs on application quit
     void OnApplicationQuit()
     {
         SaveScore();
     }
 
+    // Reset the current score and update UI
+    public void ResetScore()
+    {
+        score = 0;
+        UpdateScoreUI();
+    }
+
+    // Go to the next level and reset the current score
+    public void GoNext()
+    {
+        highScoreText.SetActive(false);
+        ResetScore();
+    }
+
+    // Display the high score in the UI
+    void DisplayHighScore()
+    {
+        if(score>highScore)
+        {
+            highScoreText.GetComponent<Text>().text = "New High Score: " + highScore;
+        }
+        else
+        {
+            highScoreText.GetComponent<Text>().text = "High Score: " + highScore;
+        }
+        highScoreText.SetActive(true);
+        
+    }
+
+    // Update the score UI with the current score
     private void UpdateScoreUI()
     {
         scoreText.GetComponent<Text>().text = "Score: " + score;
+    }
+
+    // Load the high score from PlayerPrefs
+    private void LoadScore()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore");
+    }
+
+    // Save the high score to PlayerPrefs
+    private void SaveScore()
+    {
+        PlayerPrefs.SetInt("HighScore", highScore);
     }
 }
