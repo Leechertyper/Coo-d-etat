365aaea2ca874de76bed99261ff1167f789805ce
LiamHilkewich <liamhilkewich@gmail.com>
2023-03-12 14:54:24 -0600

    Updated shop.cs to dynamically remove items from the shop if we get rid of them and add items

--- Assets/Scenes/Working Scenes/Liam_working/Shop.cs
+++ Assets/Scenes/Working Scenes/Liam_working/Shop.cs
@@ -8,69 +8,110 @@ public class Shop : MonoBehaviour
 {
     public GameObject itemButtonPrefab;
     public GameObject contentTransform;
+    public List<ShopItem> shopItemList = new List<ShopItem>();
+    private List<ShopItem> starterShopItemList = new List<ShopItem>(new ShopItem[]{
+        new ShopItem("Increase Battery", 10, "Upgrade your battery and keep your lazer powered for longer!", ""),
+        new ShopItem("Increase Health", 10, "Boost your health and increase your chances of survival!", ""),
+        new ShopItem("Increase Damage", 10, "Upgrade your weapon and deal more destruction than ever before.", ""),
+    });
 
-    public List<ShopItem> itemList = new List<ShopItem>();
-
+    // Start is called before the first frame update will create the shop items and display them
     void Start()
     {
-        int x = PlayerPrefs.GetInt("itemDataCount");
-        if(x > 0)
+        int _savedShopItemCount = PlayerPrefs.GetInt("itemDataCount");
+        for (int i = 0; i < _savedShopItemCount; i++)
         {
-            for(int i = 0; i < x; i++)
-            {
-                string itemData = PlayerPrefs.GetString("itemData"+i);
-                itemList.Add(JsonUtility.FromJson<ShopItem>(itemData));
-            }
+            string itemData = PlayerPrefs.GetString("itemData" + i);
+            shopItemList.Add(JsonUtility.FromJson<ShopItem>(itemData));
         }
-        else
-        {
-            itemList.Add(new ShopItem("Increase Battery", 10, "Upgrade your battery and keep your lazer powered for longer!", ""));
-            itemList.Add(new ShopItem("Increase Health", 10, "Boost your health and increase your chances of survival!", ""));
-            itemList.Add(new ShopItem("Increase Damage", 10, "Upgrade your weapon and deal more destruction than ever before.", ""));
 
-        }
+        CheckForNewItem();
+        CheckForOldRemovedItem();
 
         DisplayItemButtons();
     }
 
+    // will check if there is a new item added to the shop
+    private void CheckForNewItem()
+    {
+        bool exists = false;
+        foreach (ShopItem starterItem in starterShopItemList)
+        {
+            foreach (ShopItem item in shopItemList)
+            {
+                if (item.name == starterItem.name)
+                {
+                    exists = true;
+                    break;
+                }
+            }
+            if (!exists)
+            {
+                shopItemList.Add(starterItem);
+            }
+            exists = false;
+        }
+    }
+
+    // will check if there is an item removed from the shop
+    private void CheckForOldRemovedItem()
+    {
+        for (int i = 0; i < shopItemList.Count; i++)
+        {
+            bool exists = false;
+            foreach (ShopItem starterItem in starterShopItemList)
+            {
+                if (shopItemList[i].name == starterItem.name)
+                {
+                    exists = true;
+                    break;
+                }
+            }
+            if (!exists)
+            {
+                shopItemList.RemoveAt(i);
+            }
+        }
+    }
+
+    // will save the shop items when the game is closed
     void OnApplicationQuit()
     {
-        for(int i = 0; i < itemList.Count; i++)
+        PlayerPrefs.SetInt("itemDataCount", shopItemList.Count);
+        for(int i = 0; i < shopItemList.Count; i++)
         {
-            PlayerPrefs.SetString("itemData"+i, JsonUtility.ToJson(itemList[i]));
-        }
-        PlayerPrefs.SetInt("itemDataCount", itemList.Count);
-        
+            PlayerPrefs.SetString("itemData"+i, JsonUtility.ToJson(shopItemList[i]));
+        } 
     }
 
-
+    // will display the shop items
     public void DisplayItemButtons()
-{
-    foreach (ShopItem item in itemList)
     {
-        GameObject buttonObject = Instantiate(itemButtonPrefab, contentTransform.transform);
+        foreach (ShopItem item in shopItemList)
+        {
+            GameObject buttonObject = Instantiate(itemButtonPrefab, contentTransform.transform);
 
-        Text itemNameText = buttonObject.transform.Find("ItemName").GetComponent<Text>();
-        Text itemPriceText = buttonObject.transform.Find("ItemPrice").GetComponent<Text>();
-        Image itemSprite = buttonObject.transform.Find("ItemImage").GetComponent<Image>();
-        Text itemDescriptionText = buttonObject.transform.Find("ItemDescription").GetComponent<Text>();
-        Button buyButton = buttonObject.transform.Find("BuyButton").GetComponent<Button>();
+            Text itemNameText = buttonObject.transform.Find("ItemName").GetComponent<Text>();
+            Text itemPriceText = buttonObject.transform.Find("ItemPrice").GetComponent<Text>();
+            Image itemSprite = buttonObject.transform.Find("ItemImage").GetComponent<Image>();
+            Text itemDescriptionText = buttonObject.transform.Find("ItemDescription").GetComponent<Text>();
+            Button buyButton = buttonObject.transform.Find("BuyButton").GetComponent<Button>();
 
-        itemNameText.text = item.name;
-        itemPriceText.text = "$" + item.price.ToString();
-        itemSprite.sprite = Resources.Load<Sprite>(item.sprite);
-        itemDescriptionText.text = item.description;
+            itemNameText.text = item.name;
+            itemPriceText.text = "$" + item.price.ToString();
+            itemSprite.sprite = Resources.Load<Sprite>(item.sprite);
+            itemDescriptionText.text = item.description;
 
-        buyButton.onClick.AddListener(() => BuyItem(item, itemPriceText));
+            buyButton.onClick.AddListener(() => BuyItem(item, itemPriceText));
+        }
     }
-}
 
-
-public void BuyItem(ShopItem item, Text itemPriceText)
-{
-    item.price = Mathf.RoundToInt(item.price * 1.1f);
-    itemPriceText.text = "$" + item.price.ToString();
-}
+    // will buy the item and increase the price
+    public void BuyItem(ShopItem item, Text itemPriceText)
+    {
+        item.price = Mathf.RoundToInt(item.price * 1.1f);
+        itemPriceText.text = "$" + item.price.ToString();
+    }
 }
 
 [Serializable]
