ad637e1d1ab83fb850077d436fda02b48982df13
Noah V <noahvonk28@gmail.com>
2023-03-13 19:50:02 -0600

    Added Porch Pirate Script

---
+++ Assets/Scripts/Enemies/PorchPirate.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PorchPirate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // will move away from the player and spin
+    private void MoveAway()
+    {
+
+    }
+
+    // at low health it will keep spinning until it dies
+}
