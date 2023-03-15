85aed692393587f16c4286c54d2e42575e31210b
itsjorden <jcw330@usask.ca>
2023-03-14 20:26:47 -0600

    Fixed Items, and added items to enemy and boss pakage

--- Assets/Scripts/Enemies/Health.cs
+++ Assets/Scripts/Enemies/Health.cs
@@ -103,11 +103,13 @@ public class Health : MonoBehaviour
         }
         GetComponent<SpriteRenderer>().color = Color.red;
         var deathExplosion = Instantiate(deathEffects[0], transform.position, transform.rotation);
-        theDeathItems.GetComponent<deathItems>().SpawnItem(transform.position);
+        Vector3 temp = transform.position;
+        
         Destroy(deathExplosion,1.5f);
         AkSoundEngine.PostEvent("Play_Heavy_Blast", this.gameObject);
-        Destroy(gameObject);
         
+        Destroy(gameObject);
+        theDeathItems.GetComponent<deathItems>().SpawnItem(temp);
 
     }
 }
