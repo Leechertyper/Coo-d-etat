8160f06893f4fc03cd0a0fc985876fc7d61c6ded
symphonygl <roa.lindsay@usask.ca>
2023-04-03 01:33:44 -0600

    Added Pigeon Facts to the loading screen

--- Assets/Scenes/Working Scenes/Liam_working/Loading.cs
+++ Assets/Scenes/Working Scenes/Liam_working/Loading.cs
@@ -14,6 +14,7 @@ public class Loading : MonoBehaviour
     public Text downKeyText;
     public Text fireKeyText;
     public Text dashKeyText;
+    [SerializeField] public Text factsText;
 
     private void Start()
     {
@@ -21,6 +22,7 @@ public class Loading : MonoBehaviour
         loadingScreen.SetActive(true);
 
         LoadControls();
+        PigeonFacts();
 
         // Start loading the next scene in the background
         StartCoroutine(LoadNextSceneAsync());
@@ -79,5 +81,22 @@ public class Loading : MonoBehaviour
         {
             dashKeyText.text = "Dash: " + PlayerPrefs.GetString("dashKey");
         }
+    }
+
+    public void PigeonFacts() {
+
+        List<string> facts = new List<string>();
+
+        facts.Add("Pigeons can control time! They do this out of spite.");
+        facts.Add("Drones legally cannot fly within 5.6km of an airport. Pigeons, however, are above the law, and can fly wherever they want.");
+        facts.Add("There are approximately 400 million pigeons worldwide, meaning every pigeon is responible for monitoring 20 people.");
+        facts.Add("Pigeons are not yet classified as a bioweapon.");
+        facts.Add("Pigeons are regularly spotted under bridges, as many of them are employed as structural inspectors.");
+        facts.Add("The average pigeon eats 112 spiders a year. Pigeons Georg, who eats 10,000 spiders a year, is an outlier and should not be counted.");
+        facts.Add("Cuneiform was actually created by pigeons stepping on wet clay. It took the Sumerians 500 years to figure out how to read it.");
+        facts.Add("Garlic is beneficial for pigeons, as it helps them ward off vampire bats.");
+
+        var num = Random.Range(0, 7);
+        factsText.text = facts[num];
     }
 }
