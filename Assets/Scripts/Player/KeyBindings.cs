c2c4ee60e49a90dedd97c865ab505e6f67bf3651
symphonygl <roa.lindsay@usask.ca>
2023-04-03 18:14:44 -0600

    UI adjustments to options menu

--- Assets/Scripts/Player/KeyBindings.cs
+++ Assets/Scripts/Player/KeyBindings.cs
@@ -58,22 +58,19 @@ public class KeyBindings : MonoBehaviour
         switch (keyName)
         {
             case "Up":
-                upKeyText.text = "Press new key for " + keyName;
+                upKeyText.text = "Press new key:";
                 break;
             case "Down":
-                downKeyText.text = "Press new key for " + keyName;
+                downKeyText.text = "Press new key:";
                 break;
             case "Left":
-                leftKeyText.text = "Press new key for " + keyName;
+                leftKeyText.text = "Press new key:";
                 break;
             case "Right":
-                rightKeyText.text = "Press new key for " + keyName;
-                break;
-            case "Shoot":
-                fireKeyText.text = "Press new key for " + keyName;
+                rightKeyText.text = "Press new key:";
                 break;
             case "Dash":
-                dashKeyText.text = "Press new key for " + keyName;
+                dashKeyText.text = "Press new key:";
                 break;
         }
 
@@ -101,10 +98,6 @@ public class KeyBindings : MonoBehaviour
             case "Right":
                 PlayerPrefs.SetString("rightKey", newKey.ToString());
                 rightKeyText.text = "Right: " + newKey;
-                break;
-            case "Shoot":
-                PlayerPrefs.SetString("fireKey", newKey.ToString());
-                fireKeyText.text = "Shoot: " + newKey;
                 break;
             case "Dash":
                 PlayerPrefs.SetString("dashKey", newKey.ToString());
