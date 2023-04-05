c00806011d4e7d844f74d428691e4c8699593917
Leechertyper <leechertyper@gmail.com>
2023-04-05 05:05:30 -0600

    Cat was added in, some of the animations and AI are jank

--- Assets/Scenes/Working Scenes/Olamide_working/CatAI.cs
+++ Assets/Scenes/Working Scenes/Olamide_working/CatAI.cs
@@ -7,7 +7,7 @@ public class CatAI : Enemy
     public Animator animator;
     public Health hp;
 
-    [SerializeField] private int leapDistance = 20;
+    [SerializeField] private float leapDistance = 20f;
     private GlobalGrid _grid;
     private Vector2Int _gridPos;
     private Transform _target;
@@ -16,6 +16,7 @@ public class CatAI : Enemy
     private float _distY;
     private bool _awake;
     private bool _attackReady = true;
+    private bool _leapReady = true;
     enum state { Dash, Leap, Next, Wait, Attack }
 
 
@@ -50,8 +51,9 @@ public class CatAI : Enemy
 
         if (_myState == state.Next)
         {
+            animator.SetBool("IsAttacking", false);
             //Debug.Log(Vector2.Distance(_target.transform.position, transform.position));
-            if (Vector2.Distance(_target.transform.position, transform.position) < leapDistance)
+            if (Vector2.Distance(_target.transform.position, transform.position) < leapDistance && _leapReady)
             {
                 _myState = state.Leap;
                 //_myState = state.Attack;
@@ -94,10 +96,10 @@ public class CatAI : Enemy
         {
             StartCoroutine(LeapAtPlayer());
         }
-        /*if(_myState == state.Attack)
+        if(_myState == state.Attack)
         {
             StartCoroutine(catAttack());
-        }*/
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -107,13 +109,14 @@ public class CatAI : Enemy
             if (_attackReady)
             {
                 AkSoundEngine.PostEvent("Play_Cat_Swipe", this.gameObject);
+                _myState = state.Attack;
                 collision.gameObject.GetComponent<Player>().TakeDamage(BalanceVariables.catEnemy["attackDamage"] * (0.5f + (Mathf.Sqrt(GameManager.Instance.getLevelNum()) / 2)));
                 StartCoroutine(AttackCooldown(2));
             }
         }
     }
 
-    /*private IEnumerator catAttack()
+    private IEnumerator catAttack()
     {
         while(_myState == state.Attack)
         {
@@ -122,7 +125,7 @@ public class CatAI : Enemy
         }
 
         _myState = state.Next;
-    }*/
+    }
 
     private IEnumerator AttackCooldown(float time)
     {
@@ -188,15 +191,52 @@ public class CatAI : Enemy
         float timeElapsed = 0;
         float runTime = 1f;
         Vector3 inital = transform.position;
+        _distX = _target.transform.position.x - transform.transform.position.x;
+        _distY = _target.transform.position.y - transform.transform.position.y;
+
         while (timeElapsed < runTime)
         {
             transform.position = SampleParabola(inital, player, 2, timeElapsed / runTime);
+            if (Mathf.Abs(_distX) > Mathf.Abs(_distY))
+            {
+                if (_distX > 0)
+                {
+                    animator.SetInteger("Direction", 1);
+                }
+                else
+                {
+                    animator.SetInteger("Direction", 3);
+                }
+            }
+            else
+            {
+                if (_distY > 0)
+                {
+                    animator.SetInteger("Direction", 0);
+                }
+                else
+                {
+                    animator.SetInteger("Direction", 2);
+                }
+            }
             timeElapsed += Time.deltaTime;
             yield return null;
         }
         transform.position = _grid.GetTile(player, out _gridPos);
+        StartCoroutine(LeapCooldown(2f));
         _myState = state.Next;
+        
         yield break;
+    }
+
+    private IEnumerator LeapCooldown(float time)
+    {
+         _leapReady = false;
+        while (true)
+        {
+            yield return new WaitForSeconds(time);
+            _leapReady = true;
+        }
     }
 
     Vector3 SampleParabola(Vector3 start, Vector3 end, float height, float t)
