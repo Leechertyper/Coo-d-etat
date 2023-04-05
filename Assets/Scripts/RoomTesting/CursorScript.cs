c21ba7a7cefac5c4f3e750224740ccad018ded8c
Eric Behm <edbehm@gmail.com>
2023-04-04 19:46:49 -0600

    cursor to main scene + cursor remove on pause

---
+++ Assets/Scripts/RoomTesting/CursorScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetCursor();
+    }
+
+    public void UnsetCursor()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void SetCursor()
+    {
+        Cursor.SetCursor(cursorTexture, new Vector2(cursorTexture.width / 2, cursorTexture.height / 2), CursorMode.Auto);
+    }
+}
